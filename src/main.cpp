f77b6eeb82f7fab7b7fa0dc680337f456cac640d
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2025-05-11 01:41:00 +0200

    add first bgfx example

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,8 @@
 #endif
 #endif
 
+#include "logo.h"
+
 static void error_cb(int error, const char* description) {
   fprintf(stderr, "glfw error %d: %s\n", error, description);
 }
@@ -55,6 +57,8 @@ int main(int argc, char** argv) {
   bgfx::renderFrame();
 
   bgfx::Init init;
+  init.type = bgfx::RendererType::Count;
+  init.vendorId = BGFX_PCI_ID_NONE;
 
 #if BX_PLATFORM_WINDOWS
   init.platformData.nwh = glfwGetWin32Window(window);
@@ -78,8 +82,12 @@ int main(int argc, char** argv) {
 
   const bgfx::ViewId view = 0;
 
-  bgfx::setViewClear(view, BGFX_CLEAR_COLOR, 0x303030ff);
+  bgfx::setViewClear(view, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff);
   bgfx::setViewRect(view, 0, 0, width, height);
+
+  bgfx::setDebug(BGFX_DEBUG_TEXT);
+
+  printf("renderer: %s\n", bgfx::getRendererName(bgfx::getRendererType()));
 
   glfwShowWindow(window);
 
@@ -96,6 +104,27 @@ int main(int argc, char** argv) {
     }
 
     bgfx::touch(view);
+
+    bgfx::dbgTextClear();
+
+    const bgfx::Stats* stats = bgfx::getStats();
+
+    bgfx::dbgTextImage(bx::max<uint16_t>(uint16_t(stats->textWidth / 2), 20) - 20,
+                       bx::max<uint16_t>(uint16_t(stats->textHeight / 2), 6) - 6, 40, 12, s_logo, 160);
+    bgfx::dbgTextPrintf(
+        0, 1, 0x0f,
+        "Color can be changed with ANSI \x1b[9;me\x1b[10;ms\x1b[11;mc\x1b[12;ma\x1b[13;mp\x1b[14;me\x1b[0m code too.");
+
+    bgfx::dbgTextPrintf(80, 1, 0x0f,
+                        "\x1b[;0m    \x1b[;1m    \x1b[; 2m    \x1b[; 3m    \x1b[; 4m    \x1b[; 5m    \x1b[; 6m    "
+                        "\x1b[; 7m    \x1b[0m");
+    bgfx::dbgTextPrintf(80, 2, 0x0f,
+                        "\x1b[;8m    \x1b[;9m    \x1b[;10m    \x1b[;11m    \x1b[;12m    \x1b[;13m    \x1b[;14m    "
+                        "\x1b[;15m    \x1b[0m");
+
+    bgfx::dbgTextPrintf(0, 2, 0x0f, "Backbuffer %dW x %dH in pixels, debug text %dW x %dH in characters.", stats->width,
+                        stats->height, stats->textWidth, stats->textHeight);
+
     bgfx::frame();
   }
 
