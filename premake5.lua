ed045fc309c1c6f4ce23f45cef44e3732213f3c1
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2025-05-10 17:54:29 +0200

    hello bgfx

--- premake5.lua
+++ premake5.lua
@@ -6,6 +6,7 @@ workspace "bgfx-starter"
 project "bgfx-starter"
   kind "ConsoleApp"
   language "C++"
+  cppdialect "C++17"
   staticruntime "on"
 
   targetdir "%{wks.location}/bin/%{cfg.buildcfg}"
@@ -15,31 +16,74 @@ project "bgfx-starter"
     "src/**.h",
     "src/**.cpp",
 
+    -- bx
+    "vendor/bx/include/bx/*.h",
+    "vendor/bx/include/bx/inline/*.inl",
+    "vendor/bx/src/*.cpp",
+
+    -- bimg
+    "vendor/bimg/include/bimg/*.h",
+    "vendor/bimg/src/image.cpp",
+    "vendor/bimg/src/image_gnf.cpp",
+    "vendor/bimg/3rdparty/astc-encoder/source/*.cpp",
+
+    -- bgfx
+    "vendor/bgfx/include/bgfx/**.h",
+    "vendor/bgfx/src/*.h",
+    "vendor/bgfx/src/*.cpp",
+
+    -- glfw
     "vendor/glfw/src/*.h",
     "vendor/glfw/src/*.c",
   }
 
+  excludes {
+    -- bx
+    "vendor/bx/src/amalgamated.cpp",
+
+    -- bgfx
+    "vendor/bgfx/src/amalgamated.cpp",
+  }
+
   includedirs {
+    -- bx
+    "vendor/bx/include",
+    "vendor/bx/3rdparty",
+
+    -- bimg
+    "vendor/bimg/include",
+    "vendor/bimg/3rdparty/astc-encoder/include",
+
+    -- bgfx
+    "vendor/bgfx/include",
+    "vendor/bgfx/3rdparty",
+    "vendor/bgfx/3rdparty/khronos",
+    "vendor/bgfx/3rdparty/directx-headers/include/directx",
+
+    -- glfw
     "vendor/glfw/include",
   }
 
   filter "system:windows"
-    defines {
-      "_CRT_SECURE_NO_WARNINGS",
-      "_GLFW_WIN32",
-    }
-
+    defines { "_GLFW_WIN32" }
     links { "dwmapi" }
 
   filter { "system:windows", "configurations:release" }
     kind "WindowedApp"
 
+  filter { "toolset:msc*" }
+    defines { "_CRT_SECURE_NO_WARNINGS" }
+    includedirs { "vendor/bx/include/compat/msvc" }
+    buildoptions { "/Zc:__cplusplus", "/Zc:preprocessor" }
+    disablewarnings { "4244" }
+
+  filter { "toolset:msc*", "configurations:release" }
     linkoptions { "/ENTRY:mainCRTStartup" }
 
   filter "configurations:debug"
-    defines { "DEBUG" }
+    defines { "DEBUG", "BX_CONFIG_DEBUG=1" }
     symbols "on"
 
   filter "configurations:release"
-    defines { "NDEBUG" }
+    defines { "NDEBUG", "BX_CONFIG_DEBUG=0" }
     optimize "on"
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
+#include <stdio.h>
+
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
+#include <bx/bx.h>
+
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-#ifdef _WIN32
+#ifdef BX_PLATFORM_WINDOWS
 #define GLFW_EXPOSE_NATIVE_WIN32
+#endif
+
 #include <GLFW/glfw3native.h>
+
+// dark title bar
+#ifdef BX_PLATFORM_WINDOWS
 #include <dwmapi.h>
 
 #ifndef DWMWA_USE_IMMERSIVE_DARK_MODE
@@ -10,11 +22,20 @@
 #endif
 #endif
 
-int main() {
+static void error_cb(int error, const char* description) {
+  fprintf(stderr, "glfw error %d: %s\n", error, description);
+}
+
+int main(int argc, char** argv) {
+  glfwSetErrorCallback(error_cb);
+
+  glfwInitHint(GLFW_WIN32_MESSAGES_IN_FIBER, GLFW_TRUE);
+
   if (!glfwInit()) {
     return 1;
   }
 
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
   glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 
   GLFWwindow* window = glfwCreateWindow(640, 480, "hello bgfx", NULL, NULL);
@@ -29,11 +50,51 @@ int main() {
   DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &dark, sizeof(dark));
 #endif
 
+  bgfx::renderFrame();
+
+  bgfx::Init init;
+
+#if BX_PLATFORM_WINDOWS
+  init.platformData.nwh = glfwGetWin32Window(window);
+#endif
+
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+
+  init.resolution.width = width;
+  init.resolution.height = height;
+  init.resolution.reset = BGFX_RESET_VSYNC;
+
+  if (!bgfx::init(init)) {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
+  }
+
+  const bgfx::ViewId view = 0;
+
+  bgfx::setViewClear(view, BGFX_CLEAR_COLOR, 0x303030ff);
+  bgfx::setViewRect(view, 0, 0, width, height);
+
   glfwShowWindow(window);
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
+
+    int old_width = width;
+    int old_height = height;
+    glfwGetFramebufferSize(window, &width, &height);
+
+    if (width != old_width || height != old_height) {
+      bgfx::reset(width, height, BGFX_RESET_VSYNC);
+      bgfx::setViewRect(view, 0, 0, width, height);
+    }
+
+    bgfx::touch(view);
+    bgfx::frame();
   }
+
+  bgfx::shutdown();
 
   glfwDestroyWindow(window);
   glfwTerminate();
